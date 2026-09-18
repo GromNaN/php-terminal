6115092a4aeedcff640b0b76eaa8c80014bd3052
Pratik Bhujel <prateekbhujelpb@gmail.com>
2026-09-18 08:00:14 +0545

    Evolve architecture to Io\Terminal namespace, instance API, and unbacked enums (#57)
    * Evolve architecture to Io\Terminal namespace, instance API, and unbacked enums
    
    * Add PHP 8.1-8.3 compatibility macros for class registration and FENTRY
    
    * Use variadic ZEND_RAW_FENTRY macro for PHP < 8.4

--- php_terminal.h
+++ php_terminal.h
@@ -6,7 +6,7 @@
 extern zend_module_entry terminal_module_entry;
 # define phpext_terminal_ptr &terminal_module_entry
 
-# define PHP_TERMINAL_VERSION "0.7.0"
+# define PHP_TERMINAL_VERSION "0.8.0"
 
 # define TERMINAL_STREAM_STDIN 0
 # define TERMINAL_STREAM_STDOUT 1
@@ -14,6 +14,23 @@ extern zend_module_entry terminal_module_entry;
 
 # if defined(ZTS) && defined(COMPILE_DL_TERMINAL)
 ZEND_TSRMLS_CACHE_EXTERN()
+# endif
+
+# if PHP_VERSION_ID < 80400
+static zend_always_inline zend_class_entry *terminal_register_internal_class_with_flags(
+	zend_class_entry *ce,
+	zend_class_entry *parent_ce,
+	uint32_t flags
+) {
+	zend_class_entry *entry = zend_register_internal_class_ex(ce, parent_ce);
+	entry->ce_flags |= flags;
+	return entry;
+}
+#  define zend_register_internal_class_with_flags terminal_register_internal_class_with_flags
+
+#  undef ZEND_RAW_FENTRY
+#  define ZEND_RAW_FENTRY(zend_name, name, arg_info, flags, ...) \
+	{ zend_name, name, arg_info, (uint32_t) (sizeof(arg_info)/sizeof(struct _zend_internal_arg_info)-1), flags },
 # endif
 
 #endif	/* PHP_TERMINAL_H */
